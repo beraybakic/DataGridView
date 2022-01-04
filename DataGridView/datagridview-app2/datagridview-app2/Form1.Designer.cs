85fed555fd70cb9e0a42ac5f84cd52f0470a552f
beraybakic <97117149+beraybakic@users.noreply.github.com>
2022-01-04 20:04:58 +0300

    Add files via upload

---
+++ DataGridView/datagridview-app2/datagridview-app2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace datagridview_app2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.cmbIller = new System.Windows.Forms.ComboBox();
+            this.cmbIlceler = new System.Windows.Forms.ComboBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(747, 170);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(567, 229);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(206, 22);
+            this.txtAd.TabIndex = 1;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(567, 264);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(206, 22);
+            this.txtSoyad.TabIndex = 2;
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(567, 299);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(206, 22);
+            this.dtpDogumTarihi.TabIndex = 3;
+            // 
+            // cmbIller
+            // 
+            this.cmbIller.FormattingEnabled = true;
+            this.cmbIller.Location = new System.Drawing.Point(567, 334);
+            this.cmbIller.Name = "cmbIller";
+            this.cmbIller.Size = new System.Drawing.Size(206, 24);
+            this.cmbIller.TabIndex = 4;
+            // 
+            // cmbIlceler
+            // 
+            this.cmbIlceler.FormattingEnabled = true;
+            this.cmbIlceler.Location = new System.Drawing.Point(567, 371);
+            this.cmbIlceler.Name = "cmbIlceler";
+            this.cmbIlceler.Size = new System.Drawing.Size(206, 24);
+            this.cmbIlceler.TabIndex = 5;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(27, 227);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(165, 35);
+            this.btnEkle.TabIndex = 6;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(27, 281);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(165, 35);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(27, 335);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(165, 35);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.cmbIlceler);
+            this.Controls.Add(this.cmbIller);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+        private System.Windows.Forms.ComboBox cmbIller;
+        private System.Windows.Forms.ComboBox cmbIlceler;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
+
---
+++ DataGridView/datagridview-app2/datagridview-app2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace datagridview_app2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=emyo;Integrated Security=True");
+        private void vericek(string komut)
+        {
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(komut, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //dataGridView1.DataSource = dt;
+            dataGridView1.Refresh();
+            conn.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.ColumnCount = 6;
+            //dataGridView1.Columns[0].Name = "Id";
+            //dataGridView1.Columns[1].Name = "Ad";
+            //dataGridView1.Columns[2].Name = "Soyad";
+            //dataGridView1.Columns[3].Name = "Doğum Tarihi";
+            //dataGridView1.Columns[4].Name = "İl";
+            //dataGridView1.Columns[5].Name = "İlçe";
+            conn.Open();
+            SqlCommand c1 = new SqlCommand("select * from iller", conn);
+            SqlDataReader dr = c1.ExecuteReader();
+            while (dr.Read())
+            {
+                cmbIller.Items.Add(dr["il_adi"].ToString());
+            }
+            conn.Close();
+            vericek("select * from Kisiler");
+            cmbIller.SelectionChangeCommitted += CmbIller_SelectionChangeCommitted;
+        }
+
+        private void CmbIller_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmbIlceler.Items.Clear();
+            SqlCommand c2 = new SqlCommand("select a.ilce_adi from ilceler a inner join iller b on a.ilId = b.ilId where b.il_adi='"+(sender as ComboBox).SelectedItem as string+"'", conn);
+            SqlDataReader dr2 = c2.ExecuteReader();
+            while (dr2.Read())
+            {
+                cmbIlceler.Items.Add(dr2["ilce_adi"].ToString());
+            }
+            conn.Close();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            //int i = dataGridView1.Rows.Add();
+            //i = 0;
+            //dataGridView1.Rows[i].Cells[0].Value = (i + 1).ToString();
+            //dataGridView1.Rows[i].Cells[1].Value = txtAd.Text;
+            //dataGridView1.Rows[i].Cells[2].Value = txtSoyad.Text;
+            //dataGridView1.Rows[i].Cells[3].Value = dtpDogumTarihi.Value;
+            //dataGridView1.Rows[i].Cells[4].Value = cmbIller.GetItemText(cmbIller.SelectedItem);
+            //dataGridView1.Rows[i].Cells[5].Value = cmbIlceler.GetItemText(cmbIlceler.SelectedItem);
+            conn.Open();
+            SqlCommand c3 = new SqlCommand("insert into Kisiler (ad,soyad,dogum_tarihi,il,ilce) values (@ad,@soyad,@dogum_tarihi,@il,@ilce)", conn);
+            c3.Parameters.AddWithValue("@ad", txtAd.Text);
+            c3.Parameters.AddWithValue("@soyad",txtSoyad.Text);
+            c3.Parameters.AddWithValue("@dogum_tarihi",dtpDogumTarihi.Value);
+            c3.Parameters.AddWithValue("@il",cmbIller.GetItemText(cmbIller.SelectedItem));
+            c3.Parameters.AddWithValue("@ilce",cmbIlceler.GetItemText(cmbIlceler.SelectedItem));
+            c3.ExecuteNonQuery();
+            conn.Close();
+            vericek("select * from Kisiler");
+        }
+    }
+}
